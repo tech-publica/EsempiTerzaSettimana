01ec9df7fdf39eff20f43a152c3361d862bd4cc1
tech publica <tech.publica@gmail.com>
2021-07-01 14:54:56 +0200

    aggiunti esempi sugli stream

--- EsempiTerzaSettimana/src/it/epicode/lambdas/EsempiLambdaStreams.java
+++ EsempiTerzaSettimana/src/it/epicode/lambdas/EsempiLambdaStreams.java
@@ -4,18 +4,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EsempiLambdaStreams {
-	public static final Logger logger =
-			LoggerFactory.getLogger(EsempiLambdaStreams.class);
-	
+	public static final Logger logger = LoggerFactory.getLogger(EsempiLambdaStreams.class);
+
 	public static void main(String[] args) {
-		
-		
+
 		logger.debug("main starting");
 		logger.debug("main starting");
 		Runnable r = new MyRunnable();
@@ -49,16 +48,36 @@ public class EsempiLambdaStreams {
 
 			System.out.println(p);
 		});
-		
-		Stream<String> ss = persone.stream().map( p -> p.getName());
+
+		Stream<String> ss = persone.stream().map(p -> p.getName());
+
+		Stream<String> xs = persone.stream().filter(p -> p.getAge() >= 18).map(p -> p.getName());
+
+		List<String> nomi = xs.collect(Collectors.toList());
+
+		Stream<Persona> spm = persone.stream().filter(p -> p.getAge() >= 18);
+
+		List<Persona> maggiorenni = spm.collect(Collectors.toList());
+
+		List<String> nm = new ArrayList<>();
+		for (Persona p : maggiorenni) {
+			String nome = p.getName();
+			if(nome.length() > 5 ) {
+				nm.add(p.getName());
+			}
+			
+		}
+
+		List<String> nomiMaggiorenni = maggiorenni.stream().map(p -> p.getName())
+				.filter(n -> n.length() > 5).collect(Collectors.toList());
+
+		List<Integer> etaMaggiorenni = maggiorenni.stream().map(p -> p.getAge()).collect(Collectors.toList());
 
 		Consumer<Persona> cp = new PersonaConsumer();
 		persone.forEach(cp);
-		
-		
-		 Stream<String> res =  persone.stream()
-				 .flatMap( p -> p.getBestFriendNames().stream());
-		 res.forEach( s -> System.out.println(s));
+
+		Stream<String> res = persone.stream().flatMap(p -> p.getBestFriendNames().stream());
+		res.forEach(s -> System.out.println(s));
 	}
 
 	public static <T> void myForEach(List<T> elements, Consumer<T> lambda) {
--- EsempiTerzaSettimana/src/it/epicode/lambdas/Persona.java
+++ EsempiTerzaSettimana/src/it/epicode/lambdas/Persona.java
@@ -2,9 +2,16 @@ package it.epicode.lambdas;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Persona {
 	private String name;
+	private int age;
+	public static final Logger logger =
+			LoggerFactory.getLogger(EsempiLambdaStreams.class);
 	
+
 	private List<String> bestFriendNames;
 	
 	public List<String> getBestFriendNames() {
@@ -21,9 +28,13 @@ public class Persona {
 	}
 
 	public String getName() {
+		logger.debug("chiamato getName");
 		return name;
 	}
-
+	public int getAge() {
+		logger.debug("chiamato getAge");
+		return age;
+	}
 	@Override
 	public String toString() {
 		return "Persona [name=" + name + "]";
